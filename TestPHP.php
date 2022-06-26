45d30eb4713ba849bdc440abe22ab599e97b1815
0E <92708940+Yum02@users.noreply.github.com>
2022-06-26 23:25:36 +0900

    Create TestPHP.php

---
+++ TestPHP.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Hello goorm</title>
+</head>
+<body>
+	<h1>Hello goorm</h1>
+	<?php
+	$conn = mysqli_connect("localhost", "root", "123456", "testdb");
+	
+	$sql = "SELECT * FROM testtable";
+	$result = mysqli_query($conn, $sql);
+	
+	while($row = mysqli_fetch_array($result)){
+		echo "<li>{$row['id']} - {$row['title']} - {$row['description']} - {$row['created']}</li>";
+	}
+	?>
+</body>
+</html>
